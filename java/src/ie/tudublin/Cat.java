cf7722d44ca6a7915633c658629b4635d717052b
AllyannaCruz <55791523+AllyannaCruz@users.noreply.github.com>
2020-01-29 18:11:19 +0000

    killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -8,7 +8,7 @@ public class Cat extends Animal
     {
         this.numLives = lives;
     }
-    public int getNumLives()
+    public int getNumLives() //this returns value to Main()
     {
         return numLives;
     }
@@ -16,5 +16,19 @@ public class Cat extends Animal
     public void speak()
     {
         System.out.println("Meow!");
+    }
+
+    public void kill()
+    {
+        if (numLives > 0)
+        {
+            numLives = numLives - 1;
+            System.out.println("Ouch!");
+        }
+
+        if (numLives == 0)
+        {
+            System.out.println("Dead");
+        }
     }
 }
