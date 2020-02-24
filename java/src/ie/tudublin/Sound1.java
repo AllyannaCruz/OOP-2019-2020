fd1ce67f8873afe5bff14be6a5880d0f06b1743e
AllyannaCruz <55791523+AllyannaCruz@users.noreply.github.com>
2020-02-24 13:23:36 +0000

    week5 code

--- java/src/ie/tudublin/Sound1.java
+++ java/src/ie/tudublin/Sound1.java
@@ -7,7 +7,11 @@ import ddf.minim.analysis.*;
 public class Sound1 extends PApplet
 {	
 	Minim minim;
-	AudioInput ai;
+	//AudioInput ai; - microphone
+
+	as = minim.loadSample ("heroPlanet.mp3")// audio file
+	//as.trigger(); // for the audio to play
+	colorMode(HSB);
 
 	public void settings()
 	{
@@ -22,7 +26,9 @@ public class Sound1 extends PApplet
 		colorMode(HSB);
 	}
 
-	
+	//lerp (a,b,t) linear interpelation (10,20,1)
+	//lerpcircley in solution code  moves circle to make circle look better, smoother -- moves 10% closer
+
 	public void draw()
 	{	
 		background(0);		
@@ -31,12 +37,31 @@ public class Sound1 extends PApplet
 		
 		for(int i = 0 ; i < ai.bufferSize() ; i ++)
 		{
+			float average;
+			float sum = 0;
 			stroke(
 				map(i, 0, ai.bufferSize(), 0, 255)
 				, 255
 				, 255
 			);
 			line(i, cy, i, cy + ai.left.get(i) * cy);
+			sum += abs(ai.left.get(i));
+	
 		}
+		float average = sum / ai.bufferSize();
+
+		float w  = average * 1000;
+		noStroke();
+		fill (
+
+		);
+
+		float radius = average * cy;
+		ellipse(width / 2, cy, w,w); 
+	}
+
+	public void Avg() // Average of samples
+	{
+
 	}
 }
